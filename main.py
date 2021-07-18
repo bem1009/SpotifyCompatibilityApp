48aadd049763860901b96c645ffec54ed90e45bb
Ben Miller <bemiller1009@gmail.com>
2021-07-17 23:41:55 -0400

    Added functionality to create playlist and add songs

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import requests
 import re 
 import json
-from secrets import spotify_user_id, spotify_token
+import urllib
 
 from requests.api import request
 
 USER_IDS = ["https://api.spotify.com/v1/users/bmiller1550/playlists?limit=20", "https://api.spotify.com/v1/users/21o5gb3zdgw7grpnlha7sfu5y/playlists?limit=20",
             "https://api.spotify.com/v1/users/21o5gb3zdgw7grpnlha7sfu5y/playlists?limit=15&offset=20", "https://api.spotify.com/v1/users/tlounsy/playlists?limit=20"]
-ACCESS_TOKEN = "BQDL3-LyanZ274Gw7FTdlbEa-RVlFfiB3G-yoMwwf1gliALsQxPsgv_ng5tpphoJkjODoDWd0x2nbzVhTuemTZ2LkFtBbKTCMypoIAT1cEmnyAZY4VYgbSFnWRsgVYFfTOakXxnizcanYqo10AuJYUrZyawXvUQ"
+
+Profile = "https://api.spotify.com/v1/users/bmiller1550/playlists"
+ACCESS_TOKEN = "BQAc-lpWUhj7124PCoZNleqDWrrszJqKUOs-Uc-GBqgW_MvwYp5ZRMZA_Vu1V7NFy9H1OL7iWkcAhjdK2zgRuzH1B3ztUBvLVAW7t3lWAxTiWLum0U-Hv4_SRx_V19lEk6EtkiRkPKPnPCnOXyTKL2A29cO1xYYTFf490JmyNxPqCB798yfQXUCsmk1zMDN7RNSX_-RgKsrRhXyiEo1cNZHiUZ8"
 
 
-#FIXES - For some reason the "New Music Friday" Does not work. Every other playlist works however
+#FIXES - For some reason the "New Music Friday" Does not work. Every other playlist I've tried works 
 
 def create_playlist_on_spotify(name, public):
     response = requests.post(
-        USER_ID,
+        Profile,
         headers={
             "Authorization": f"Bearer {ACCESS_TOKEN}" 
         },
@@ -26,7 +28,51 @@ def create_playlist_on_spotify(name, public):
         }
     )
     json_resp = response.json()
-    return json_resp
+    return json_resp["id"]
+
+
+def get_spotify_uri(artist,song):
+
+    query = urllib.parse.quote(f"{artist} {song}")
+    url = f"https://api.spotify.com/v1/search?q={query}&type=track"
+
+    response = requests.get(
+        url,
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {ACCESS_TOKEN}"
+        }
+
+    )
+    response_json = response.json()
+    results = response_json["tracks"]["items"]
+
+    if results:
+        return results[0]["id"]
+    
+    else:
+        return None 
+
+
+def add_song_to_playlist(playlist_id, song_uri):
+
+    query = urllib.parse.quote(f"spotify:track:{song_uri}")
+    url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks?uris={query}"
+
+    response = requests.post(
+        url,
+        json = {
+            "ids": song_uri
+        },
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {ACCESS_TOKEN}"
+        }
+    )
+    response.json = response.json()
+    
+    return response.ok
+
 
 
 def get_user_playlists(USER_ID):
@@ -78,29 +124,10 @@ def get_playlist_songs(playlist_urls,songcount):
 
     return songs
 
-def create_playlist(songs):
-    request_body = json.dumps({
-        "name": "PlayList!",
-        "description": "All Compatible Spotify Songs",
-        "public" : True 
 
-    })
 
-    query = "https://api.spotify.com/v1/users/bmiller1550/playlists"
 
-    response = requests.post(
-        query,
-        data = request_body,
-        headers={
-            "Content-Type":"application/json",
-            "Authorization":"Bearer {}".format(spotify_token)
-
-        }
-    )
-    response_json = response.json()
-
-    #return playlist ID
-    return response_json["id"]
+    
 
 
 def main():
@@ -167,6 +194,16 @@ def main():
         print(song)
 
     print(len(crossedSongs))
+
+    #find all the URIs for all the cross-referenced songs you found 
+    uris = []
+    playlist_identifier = create_playlist_on_spotify("New Playlist!",False)
+    for i in range(len(crossedSongs)):
+        uris = get_spotify_uri(crossedSongs[i][0], crossedSongs[i][1]) 
+        add_song_to_playlist(playlist_identifier,uris)
+        
+    
+    print(playlist_identifier)
     
 
 if __name__ == '__main__':
