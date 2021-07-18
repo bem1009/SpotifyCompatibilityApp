d0d27041e3e095190a1a4e4a4a1e2f6010642e55
Ben Miller <bemiller1009@gmail.com>
2021-07-18 16:20:11 -0400

    Text Box adding works fully now

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 var button = document.getElementById("addProfiles");
+var totalTextBoxes = 1;
+
+
 
 button.onclick = function addTextBoxes(){
-    console.log("Button Clicked!")
+    console.log("Button Clicked!");
 
+    if (totalTextBoxes == 4) {
+        return
+    }
+
+    totalTextBoxes += 1;
+    var ptag = document.createElement("p");
     // Create an input type dynamically
     var element = document.createElement("input");
     element.type = "text";
 
     //Create Labels
     var label = document.createElement("Label");
-    label.innerHTML = "New Label";   
+    label.innerHTML = "New Label ";   
 
     // 'foobar' is the div id, where new fields are to be added
     var foo = document.getElementById("inputProfiles");
 
-    foo.appendChild(label);
-    foo.appendChild(element);
+    foo.appendChild(ptag);
+
+    ptag.appendChild(label);
+    ptag.appendChild(element);
 
 };
