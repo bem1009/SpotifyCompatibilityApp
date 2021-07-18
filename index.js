72ad714453782c22c44bfbf3beb2792b033d8d7f
Ben Miller <bemiller1009@gmail.com>
2021-07-18 17:04:33 -0400

    Pressing Submit button now grabs all the data from the text boxes

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
-var button = document.getElementById("addProfiles");
+var addProfileButton = document.getElementById("addProfiles");
 var totalTextBoxes = 1;
+var submitButton = document.getElementById("passDataToScript");
+
+var textBoxIDs = ["fname1"];
 
 
+submitButton.onclick = function processData(){
+    
+    //console.log("Input is " + profile );
+    var profiles = [];
+    for (var i = 0; i < textBoxIDs.length; i++ ) {
+        console.log("Input is " + textBoxIDs[i] );
 
-button.onclick = function addTextBoxes(){
-    console.log("Button Clicked!");
+        profiles.push(document.getElementById(textBoxIDs[i]).value);
+    }
+}
+
+addProfileButton.onclick = function addTextBoxes(){
 
     if (totalTextBoxes == 4) {
         return
     }
 
-    totalTextBoxes += 1;
+    
     var ptag = document.createElement("p");
     // Create an input type dynamically
     var element = document.createElement("input");
     element.type = "text";
-
+    totalTextBoxes += 1;
+    element.id = "fname" + totalTextBoxes;
+    textBoxIDs.push(element.id)
     //Create Labels
     var label = document.createElement("Label");
-    label.innerHTML = "New Label ";   
+    label.innerHTML = "User-ID: ";   
 
     // 'foobar' is the div id, where new fields are to be added
     var foo = document.getElementById("inputProfiles");
@@ -27,5 +41,7 @@ button.onclick = function addTextBoxes(){
 
     ptag.appendChild(label);
     ptag.appendChild(element);
+
+    
 
 };
